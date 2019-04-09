0fe74aaadb923ff8c8ec668fe764ed0c71ee8737
Alistair Brice <s5092549@g23p220m061996.local>
2019-04-09 12:34:36 +1000

    Initial Commit

---
+++ Milestone1/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Milestone1
+//
+//  Created by Alistair Brice on 9/4/19.
+//  Copyright © 2019 Alistair Brice. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
